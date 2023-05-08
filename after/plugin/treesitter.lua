67b3bc5a2e04014934867b22726b45a79f6295ec
Wojciech Piętka <wojciech.pietka13@gmail.com>
2023-05-08 12:15:06 +0200

    *

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'c', 'cpp', 'rust', 'go', 'lua', 'vim', 'yaml', 'json', -- one of 'all', 'maintained' (parsers with maintainers), or a list of languages
+  ensure_installed = 'c', 'cpp', 'rust', 'go', 'lua', 'vim', 'yaml', 'json', 'typescript', 'html', 'css',
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
-vim.g.mapleader      = ' '
-vim.o.splitright     = true
-vim.o.splitbelow     = true
-vim.o.list           = true
-vim.o.number         = true
-vim.o.relativenumber = true
-vim.o.ignorecase     = true
+vim.g.mapleader         = ' '
+vim.g.netrw_localrmdir  = 'rm -rf'
+vim.g.move_key_modifier = 'C'
+vim.o.splitright        = true
+vim.o.splitbelow        = true
+vim.o.list              = true
+vim.o.number            = true
+vim.o.relativenumber    = true
+vim.o.ignorecase        = true
 
 vim.keymap.set('n', '<Leader>s' , ':w<CR>')
-vim.keymap.set('n', '<A-J>'     , ':tabnext<CR>')
-vim.keymap.set('n', '<A-K>'     , ':tabprev<CR>')
 vim.keymap.set('n', '<C-w>/'    , ':vsplit<CR>')
 vim.keymap.set('n', '<C-w>-'    , ':split<CR>')
 vim.keymap.set('n', '<Right>'   , '<C-w>>')
@@ -20,13 +20,12 @@ vim.keymap.set('n', 'gss'       , ':source<CR>')
 vim.keymap.set('n', 'E'         , ':Ex /home/wojtek/.config/nvim/<CR>')
 vim.keymap.set('n', '<Leader>cd', ':cd %:h/')
 vim.keymap.set('n', '<Leader>tb', ':Tabularize /')
+vim.keymap.set('v', '<Leader>tb', ':Tabularize /')
 vim.keymap.set('i', 'jk'        , '<Esc>')
 vim.keymap.set('t', 'jk'        , '<C-\\><C-n>')
-vim.keymap.set('t', '<A-J>'     , '<C-\\><C-n>:tabnext<CR>')
-vim.keymap.set('t', '<A-K>'     , '<C-\\><C-n>:tabprev<CR>')
 
 vim.cmd('autocmd BufEnter *.go nnoremap <F1> :!go run .<CR>')
-vim.cmd('autocmd BufWritePost *.go :silent !go fmt')
+-- vim.cmd('autocmd BufWritePost *.go :silent !go fmt')
 vim.cmd('autocmd BufEnter *.rs nnoremap <F1> :!cargo run<CR>')
 
 local Plug = vim.fn['plug#']
