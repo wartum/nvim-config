499c70b62653be8aecb2021da4a44917b5ed2925
Wojciech Piętka <wojciech.pietka13@gmail.com>
2023-09-03 18:56:28 +0200

    nc

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,12 @@
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {
+            "wwwroot",
+            "obj",
+            "bin",
+            "build",
+        }
+    }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
