0b3f8f298d6b71a06861e5f0407b596c5501e05a
Wojciech Piętka <wojciech.pietka13@gmail.com>
2023-09-05 08:02:26 +0200

    nc

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,7 +13,8 @@ local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
   mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
   }
 })
 
